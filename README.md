d14cfe90e1cefe1260b1fe616ce21dd3b032e68e
alvaromm14 <49958868+alvaromm14@users.noreply.github.com>
2025-11-08 10:23:01 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# 15 South-South Reports in 25 years of Development Cooperation
+
+This infographic is included in the 15th edition of the "[Report on South-South and Triangular Cooperation in Ibero-America.](https://informesursur.org/es/report/work-in-progress-report-on-south-south-and-triangular-cooperation-in-ibero-america-2024/)."
+
+The infographic presents a timeline, from 2000 to 2025, which summarizes 25 years between two milestones of the International Development Agenda: the Millennium Summit in New York and the Fourth Conference on Financing for Development, scheduled to be held in Seville (Spain).
+
+This chronology highlights the origin of the Report, its evolution and main achievements (in the upper part of the infographic) while visibly connecting this information with the global development and SSTC agendas, both illustrated through the most relevant events of this first quarter century (lower part).
+
+
+
+
---
+++ src/components/ArrowText.svelte
@@ -0,0 +1,177 @@
+<script>
+    import { fade } from "svelte/transition";
+
+    export let innerWidth;
+    export let height;
+    export let datos;
+    export let vertical = false;
+
+    // Coordenadas base según orientación
+    $: firstPos = vertical ? 10 : (0 / (datos.length - 1)) * innerWidth;
+
+    $: center = vertical ? innerWidth / 2 : height / 2.5;
+
+    export let show = true;
+
+    const textHeight = 28;
+    const arrowLength = 35;
+</script>
+
+<svg>
+    {#if show}
+        <!-- Texto superior / izquierdo -->
+        <g
+            in:fade={{ delay: 1000, duration: 500 }}
+            out:fade={{ duration: 300 }}
+        >
+            {#if vertical}
+                <text
+                    y={firstPos}
+                    fill="#589ea4"
+                    font-size="13"
+                    font-weight="600"
+                    font-style="italic"
+                    text-anchor="middle"
+                >
+                    <tspan x={center / 2.5} dy="0">Report of South- </tspan>
+                    <tspan x={center / 2.5} dy="1.2em">
+                        South and Triangular</tspan
+                    >
+                    <tspan x={center / 2.5} dy="1.2em">Cooperation</tspan>
+                    <tspan x={center / 2.5} dy="1.2em">in Ibero-America</tspan>
+                </text>
+
+                <!-- Flecha vertical hacia abajo -->
+                <path
+                    d="M {center / 2.5},{64} L {center / 2.5},{firstPos +
+                        textHeight +
+                        arrowLength}"
+                    stroke="#589ea4"
+                    stroke-width="1"
+                    fill="none"
+                />
+                <path
+                    d="M {center / 2.5 - 5},{firstPos +
+                        textHeight +
+                        arrowLength} L {center / 2.5 + 5},{firstPos +
+                        textHeight +
+                        arrowLength} L {center / 2.5},{firstPos +
+                        textHeight +
+                        arrowLength +
+                        5} Z"
+                    fill="#589ea4"
+                />
+            {:else}
+                <!-- Horizontal -->
+                <path
+                    d="M {firstPos},{center - 75} L {firstPos + 80},{center -
+                        75}"
+                    stroke="#589ea4"
+                    stroke-width="1"
+                    fill="none"
+                />
+                <path
+                    d="M {firstPos + 75},{center - 80} L {firstPos +
+                        80},{center - 75} L {firstPos + 75},{center - 70} Z"
+                    fill="#589ea4"
+                />
+                <text
+                    x={firstPos - 50}
+                    y={center - 105}
+                    fill="#589ea4"
+                    font-size="13"
+                    font-weight="600"
+                    font-style="italic"
+                    text-anchor="start"
+                >
+                    <tspan x={firstPos} dy="0"
+                        >Report of South-South and Triangular</tspan
+                    >
+                    <tspan x={firstPos} dy="1.2em">
+                        Cooperation in Ibero-America</tspan
+                    >
+                </text>
+            {/if}
+        </g>
+
+        <!-- Texto inferior / derecho -->
+        <g
+            in:fade={{ delay: 1000, duration: 500 }}
+            out:fade={{ duration: 300 }}
+        >
+            {#if vertical}
+                <text
+                    y={firstPos}
+                    fill="#ecba56"
+                    font-size="13"
+                    font-weight="600"
+                    font-style="italic"
+                    text-anchor="middle"
+                >
+                    <tspan x={center + center / 1.5} dy="0"
+                        >Global Development,</tspan
+                    >
+                    <tspan x={center + center / 1.5} dy="1.2em"
+                        >South-South and</tspan
+                    >
+                    <tspan x={center + center / 1.5} dy="1.2em">
+                        Triangular cooperation
+                    </tspan>
+                    <tspan x={center + center / 1.5} dy="1.2em"> Agenda</tspan>
+                </text>
+
+                <!-- Flecha vertical hacia abajo -->
+                <path
+                    d="M {center + center / 1.5},{64} L {center +
+                        center / 1.5},{firstPos + textHeight + arrowLength}"
+                    stroke="#ecba56"
+                    stroke-width="1"
+                    fill="none"
+                />
+                <path
+                    d="M {center + center / 1.5 - 5},{firstPos +
+                        textHeight +
+                        arrowLength} L {center + center / 1.5 + 5},{firstPos +
+                        textHeight +
+                        arrowLength} L {center + center / 1.5},{firstPos +
+                        textHeight +
+                        arrowLength +
+                        5} Z"
+                    fill="#ecba56"
+                />
+            {:else}
+                <path
+                    d="M {firstPos},{center + 75} L {firstPos + 80},{center +
+                        75}"
+                    stroke="#ecba56"
+                    stroke-width="1"
+                    fill="none"
+                />
+                <path
+                    d="M {firstPos + 75},{center + 70} L {firstPos +
+                        80},{center + 75} L {firstPos + 75},{center + 80} Z"
+                    fill="#ecba56"
+                />
+                <text
+                    x={firstPos + 85}
+                    y={center + 96}
+                    fill="#ecba56"
+                    font-size="13"
+                    font-weight="600"
+                    font-style="italic"
+                    text-anchor="start"
+                >
+                    <tspan x={firstPos} dy="0"
+                        >International Development Agenda</tspan
+                    >
+                    <tspan x={firstPos} dy="1.2em"
+                        >and Global Agenda on South-South</tspan
+                    >
+                    <tspan x={firstPos} dy="1.2em">
+                        and Triangular Cooperation</tspan
+                    >
+                </text>
+            {/if}
+        </g>
+    {/if}
+</svg>
